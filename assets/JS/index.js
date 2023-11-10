545f2705c20b602018da7d2a3dc0e96da2b98574
medlinnaa <madinamamishova@gmail.com>
2023-11-10 20:21:31 +0400

    mega dropdown menu completed

---
+++ assets/JS/index.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navbar");
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+
+document.querySelectorAll(".navbar").forEach(n =>
+    n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
+    
